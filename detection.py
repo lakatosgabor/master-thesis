395ee7bb338f2f57c18c9100d761efcd9fa96afd
Gabor Lakatos <65727400+lakatosgabor@users.noreply.github.com>
2023-01-31 19:17:39 +0100

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,14 +1,13 @@
 import numpy as np
 import cv2
 import pickle
-import serial
 from pyfirmata import Arduino, SERVO
 from time import sleep
 
 frameWidth = 640
 frameHeight = 480
 brightness = 180
-threshold = 0.2
+threshold = 0.8
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 cap = cv2.VideoCapture(0)
@@ -18,35 +17,6 @@ cap.set(10, brightness)
 
 pickle_in = open("model_trained.p", "rb")  ## rb = READ BYTE
 model = pickle.load(pickle_in)
-
-port = 'COM3'
-pin = 3
-board = Arduino(port)
-board.digital[pin].mode = SERVO
-
-#ser = serial.Serial('COM3', 9600)
-
-def rotateServo(pin):
-    board.digital[pin].write(0)
-    sleep(0.5)
-
-    board.digital[pin].write(90)
-    sleep(0.5)
-
-    board.digital[pin].write(180)
-    sleep(0.5)
-
-    board.digital[pin].write(90)
-    sleep(0.5)
-
-    board.digital[pin].write(0)
-    sleep(0.5)
-
-def led_on_off(ser, isOn):
-    if isOn == True:
-        ser.write(b'H')
-    elif isOn == False:
-        ser.write(b'L')
 
 def grayscale(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -61,7 +31,7 @@ def preprocessing(img):
     return img
 
 def getCalssName(classNo):
-    if classNo == 35 or classNo == 13 or classNo == 34:
+    if classNo == 0:
         return 'Algopirin'
     return 'Searching...'
 
@@ -70,10 +40,10 @@ while True:
     success, imgOrignal = cap.read()
 
     img = np.asarray(imgOrignal)
-    img = cv2.resize(img, (32, 32))
+    img = cv2.resize(img, (200, 200))
     img = preprocessing(img)
     cv2.imshow("Processed Image", img)
-    img = img.reshape(1, 32, 32, 1)
+    img = img.reshape(1, 200, 200, 1)
 
 
     imgOrignal = cv2.resize(imgOrignal, (600, 600))
@@ -89,20 +59,9 @@ while True:
 
     if probabilityValue > threshold:
         if (str(getCalssName(classIndex)) == 'Algopirin'):
-            print('---------------------------------------------------##########################--------------------------------------')
-            print(str(getCalssName(classIndex)) + ' találat' + ' ' + str(classIndex))
-            board.digital[pin].write(180)
-            sleep(0.5)
-            board.digital[pin].write(90)
-            sleep(0.5)
-            break
-
+            print(str(getCalssName(classIndex)) + ' találat' + ' Class: ' + str(classIndex) + ' probabilityValue: ' + str(probabilityValue))
 
     key = cv2.waitKey(1)
     if key == ord("q"):
-        #led_on_off(ser, False)
-        board.digital[pin].write(90)
         sleep(0.5)
         break
-
-    #led_on_off(ser, True)
