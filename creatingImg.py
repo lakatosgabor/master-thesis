7c1a83131baf29e530b15bce6a5d540b52890bf6
Gabor Lakatos <65727400+lakatosgabor@users.noreply.github.com>
2023-01-31 19:18:28 +0100

    Update creatingImg.py

--- creatingImg.py
+++ creatingImg.py
@@ -1,24 +1,30 @@
 import cv2
 import numpy as np
 
+
 medName = 'algopirin'
-numberOfImage = 0
+camera = cv2.VideoCapture(0)
 
-camera = cv2.VideoCapture(1)
-while True:
-    success, imgOrignal = camera.read()
-    createdDataImage = np.asarray(imgOrignal)
-    createdDataImage = cv2.resize(createdDataImage, (500, 500))
-    cv2.rectangle(createdDataImage, (167, 221), (226, 300), (255, 0, 0), 3)
-    cv2.imshow("Set Image", createdDataImage)
+def createImg(medName, camera):
+    numberOfImage = 0
 
-    key = cv2.waitKey(1)
-    if key == ord("k"):
-        cv2.rectangle(createdDataImage, (167, 221), (226, 300), (0, 255, 0), 3)
-        croppedImage = createdDataImage[221:300, 167:226]
-        cv2.imwrite("Images/" + medName + "-" + str(numberOfImage) + ".jpg", croppedImage)
-        cv2.putText(createdDataImage, 'Images:  ' + str(numberOfImage), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 3)
-        cv2.imshow("Make Image", createdDataImage)
-        numberOfImage += 1
-    elif key == ord("q"):
-        break
+    while True:
+        success, imgOrignal = camera.read()
+        createdDataImage = np.asarray(imgOrignal)
+        createdDataImage = cv2.resize(createdDataImage, (500, 500))
+        cv2.rectangle(createdDataImage, (167, 221), (226, 300), (255, 0, 0), 3)
+        cv2.imshow("Set Image", createdDataImage)
+
+        key = cv2.waitKey(1)
+        if key == ord("k"):
+            cv2.rectangle(createdDataImage, (167, 221), (226, 300), (0, 255, 0), 3)
+            croppedImage = createdDataImage[221:300, 167:226]
+            cv2.imwrite("Images/" + medName + "-" + str(numberOfImage) + ".jpg", croppedImage)
+            cv2.putText(createdDataImage, 'Images:  ' + str(numberOfImage), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 3)
+            cv2.imshow("Make Image", createdDataImage)
+            numberOfImage += 1
+        elif key == ord("q"):
+            break
+
+
+createImg(medName, camera)
